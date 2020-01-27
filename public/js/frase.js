30815e6ed4f8db61371ede640b64061f8fa4b7fe
Bruno Santana <brunoalexandre313@gmail.com>
2020-01-27 13:23:45 -0300

    Adiciona e organiza lógica para a requisição ajax

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,13 +1,11 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
+  spinner();
   $.get("http://localhost:3000/frases", trocaFraseAleatoria)
-  .fail(function(){
-    $("#erro").toggle();
-    setTimeout(function(){
-      $("#erro").toggle();
-    }, 2000);
-  });
+  .fail(falhaCarregamento)
+  .always(spinner);
 }
 
 function trocaFraseAleatoria(data) {
@@ -17,4 +15,33 @@ function trocaFraseAleatoria(data) {
   frase.text(data[numRand].texto);
   atualizaTamanhoFrase();
   atualizaTempoInicial(data[numRand].tempo);
+}
+
+function buscaFrase() {
+  var fraseId = $("#frase-id").val();
+  var requisicao = {id: fraseId};
+
+  spinner();
+  $.get("http://localhost:3000/frases", requisicao, trocaFrase)
+  .fail(falhaCarregamento)
+  .always(spinner);
+}
+
+function trocaFrase(resposta) {
+  var frase = $(".frase");
+
+  frase.text(resposta.texto);
+  atualizaTamanhoFrase();
+  atualizaTempoInicial(resposta.tempo);
+}
+
+function spinner() {
+  $("#spinner").toggle();
+}
+
+function falhaCarregamento() {
+  $("#erro").toggle();
+  setTimeout(function(){
+    $("#erro").toggle();
+  }, 2000);
 }
